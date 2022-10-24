ee6391701f627a884f702d2da71ac4ea618987b8
MarvinAichinger <marvin.aichinger@outlook.com>
2022-10-24 12:00:59 +0200

    nfc test

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-//url: https://raw.githubusercontent.com/MarvinAichinger/MarvinAichinger.github.io/master/ff-fragenkatalog/fragen.txt
+let writeButton = document.querySelector("#write");
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    xhr = null;
+writeButton.addEventListener("click", async () => {
+  console.log("User clicked write button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.write("Hello world!");
+    console.log("> Message written");
+  } catch (error) {
+    console.log("Argh! " + error);
   }
-  return xhr;
-}
-
-function makeCorsRequest() {
-  var url = "https://raw.githubusercontent.com/MarvinAichinger/MarvinAichinger.github.io/master/ff-fragenkatalog/fragen.txt";
-
-  var xhr = createCORSRequest("GET", url);
-  if (!xhr) {
-    alert("CORS not supported");
-    return;
-  }
-
-  xhr.onload = function () {
-    var text = xhr.responseText;
-    //console.log(text)
-    readQuestions(text)
-  };
-
-  xhr.onerror = function () {
-    alert("Woops, there was an error making the request.");
-  };
-
-  xhr.send();
-}
-
-function readQuestions(text) {
-
-    let arr = text.split("---")
-
-}
-
-
-//start
-makeCorsRequest()
+});
